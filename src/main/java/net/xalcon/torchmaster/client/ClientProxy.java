3e974a8551247d070e9e8b61e6504f89a4b822d5
Xalcon <xalcon@xalcon.net>
2017-06-25 21:54:35 +0200

    Updated forge version

--- src/main/java/net/xalcon/torchmaster/client/ClientProxy.java
+++ src/main/java/net/xalcon/torchmaster/client/ClientProxy.java
@@ -1,13 +1,6 @@
 package net.xalcon.torchmaster.client;
 
-import net.minecraft.client.renderer.block.model.ModelResourceLocation;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemBlock;
-import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.client.model.ModelLoader;
-import net.minecraftforge.fml.common.registry.ForgeRegistries;
 import net.xalcon.torchmaster.common.CommonProxy;
-import net.xalcon.torchmaster.common.blocks.BlockBase;
 
 public class ClientProxy extends CommonProxy
 {
