fabc225261dc63ccee31f20667126e32efac77c4
Chloe Dawn <chloe@sapphic.dev>
2020-07-07 16:40:35 +0100

    Fix missing localization and add beginner tooltips (#128)
    * Fix missing localization and add beginner tooltips
    
    * Update en_us.json
    
    update megatorch to use the same wording as dreadlamp since their size is the same by default for some time now
    
    Co-authored-by: Xalcon <Xalcon@users.noreply.github.com>

--- src/main/java/net/xalcon/torchmaster/common/items/TMItemBlock.java
+++ src/main/java/net/xalcon/torchmaster/common/items/TMItemBlock.java
@@ -1,7 +1,16 @@
 package net.xalcon.torchmaster.common.items;
 
 import net.minecraft.block.Block;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.item.BlockItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.world.World;
+import net.xalcon.torchmaster.TorchmasterConfig;
+
+import javax.annotation.Nullable;
+import java.util.List;
 
 public class TMItemBlock extends BlockItem
 {
@@ -9,5 +18,13 @@ public class TMItemBlock extends BlockItem
     {
         super(blockIn, builder);
         this.setRegistryName(blockIn.getRegistryName());
+    }
+
+    @Override
+    public void addInformation(ItemStack stack, @Nullable World world, List<ITextComponent> tooltip, ITooltipFlag flag)
+    {
+        super.addInformation(stack, world, tooltip, flag);
+        if(TorchmasterConfig.GENERAL.beginnerTooltips.get())
+            tooltip.add(new TranslationTextComponent(this.getTranslationKey(stack) + ".tooltip"));
     }
 }
