c927525657933654649be3ec3a8d2e8211818779
Xalcon <Xalcon@users.noreply.github.com>
2024-09-05 17:05:12 +0200

    downgrade to mc 1.21.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## v21.0.2
+- Downgrade to MC 1.21 for fabric compatibility
+- Improve blocking logic
+
 ## v21.0.1 
 - Fix invisible light blocks not being replaceable by other blocks ([#234](https://github.com/Xalcon/TorchMaster/issues/234))
 - Update to MC 1.21.1
--- common/src/main/java/net/xalcon/torchmaster/events/TorchmasterEventHandler.java
+++ common/src/main/java/net/xalcon/torchmaster/events/TorchmasterEventHandler.java
@@ -14,15 +14,74 @@ import java.util.function.BooleanSupplier;
 
 public class TorchmasterEventHandler
 {
+    private static boolean isIntentionalSpawn(MobSpawnType spawnType)
+    {
+        switch(spawnType)
+        {
+            case BREEDING:
+            case DISPENSER:
+            case BUCKET:
+            case CONVERSION:
+            case SPAWN_EGG:
+            case TRIGGERED:
+            case COMMAND:
+            case EVENT:
+                return true;
+            case NATURAL:
+            case CHUNK_GENERATION:
+            case PATROL:
+            case SPAWNER:
+            case STRUCTURE:
+            case MOB_SUMMONED:
+            case REINFORCEMENT:
+            case JOCKEY:
+            default:
+                return false;
+        }
+    }
+
+    private static boolean isNaturalSpawn(MobSpawnType spawnType)
+    {
+        switch(spawnType)
+        {
+            case NATURAL:
+            case CHUNK_GENERATION:
+            case PATROL: // Patrol can be considered natural
+            default:
+                return true;
+            case BREEDING:
+            case CONVERSION:
+            case BUCKET:
+            case DISPENSER:
+            case SPAWNER:
+            case STRUCTURE:
+            case MOB_SUMMONED:
+            case JOCKEY:
+            case REINFORCEMENT:
+            case TRIGGERED:
+            case SPAWN_EGG:
+            case COMMAND:
+            case EVENT:
+                return false;
+        }
+    }
+
     public static void onCheckSpawn(final MobSpawnType spawnType, final Entity entity, final Vec3 location, final EventResultContainer container)
     {
         var config = Services.PLATFORM.getConfig();
         Torchmaster.LOG.debug("CheckSpawn - Reason: {}, Type: {}, Pos: {}/{}/{}", spawnType, EntityType.getKey(entity.getType()), location.x, location.y, location.z);
-        // Respect other mods decisions to allow a spawn if enabled in config
-        if(!config.getAggressiveSpawnChecks() && container.getResult() == EventResult.ALLOW) return;
-        // check if non-natural spawns should be blocked
-        if(config.getBlockOnlyNaturalSpawns() && spawnType == MobSpawnType.SPAWNER) return;
-        if(spawnType != MobSpawnType.NATURAL && spawnType != MobSpawnType.STRUCTURE) return;
+
+        // Check if the spawn was intentional (i.e. player invoked), we dont block those
+        if(isIntentionalSpawn(spawnType))
+            return;
+
+        // If aggressive spawn checks are disabled, check if other mods already explicitly allowed the spawn
+        if(!config.getAggressiveSpawnChecks() && container.getResult() == EventResult.ALLOW)
+            return;
+
+        // Check if we should block artificial spawns
+        if(config.getBlockOnlyNaturalSpawns() && !isNaturalSpawn(spawnType))
+            return;
 
         var level = entity.getCommandSenderWorld();
 
@@ -33,32 +92,6 @@ public class TorchmasterEventHandler
                 container.setResult(EventResult.DENY);
                 Torchmaster.LOG.debug("Blocking spawn of {}", EntityType.getKey(entity.getType()));
                 //event.getEntity().addTag("torchmaster_removed_spawn");
-            }
-            else
-            {
-                Torchmaster.LOG.debug("Allowed spawn of {}", EntityType.getKey(entity.getType()));
-            }
-        });
-    }
-
-    public static void onDoSpecialSpawn(final MobSpawnType spawnType, final Entity entity, final Vec3 location, final EventResultContainer container)
-    {
-        var config = Services.PLATFORM.getConfig();
-        Torchmaster.LOG.debug("DoSpecialSpawn - Reason: {}, Type: {}, Pos: {}/{}/{}", spawnType, EntityType.getKey(entity.getType()), location.x, location.y, location.z);
-        // Respect other mods decisions to allow a spawn if enabled in config
-        if(!config.getAggressiveSpawnChecks() && container.getResult() == EventResult.ALLOW) return;
-        // check if non-natural spawns should be blocked
-        if(config.getBlockOnlyNaturalSpawns() && spawnType == MobSpawnType.SPAWNER) return;
-        if(spawnType != MobSpawnType.NATURAL && spawnType != MobSpawnType.STRUCTURE) return;
-
-        var world = entity.getCommandSenderWorld();
-
-        Torchmaster.getRegistryForLevel(world).ifPresent(reg ->
-        {
-            if(reg.shouldBlockEntity(entity, entity.getCommandSenderWorld(), spawnType))
-            {
-                container.setResult(EventResult.DENY);
-                Torchmaster.LOG.debug("Blocking spawn of {}", EntityType.getKey(entity.getType()));
             }
             else
             {
